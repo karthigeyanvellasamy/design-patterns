7544ff054ef3f608a705c264ed22552fe1b18b79
Karthigeyan vellasamy <v.karthigeyan@hotmail.com>
2022-08-04 23:28:48 +0530

    Singleton Pattern - refactor changes

--- src/com/pattern/creational/MainClass.java
+++
@@ -1,8 +0,0 @@
-package com.pattern.creational;
-
-public class MainClass {
-	
-	public static void main(String[] args) {
-		System.out.println("Creational Pattern");
-	}
-}
---
+++ src/com/pattern/creational/SingletonPattern.java
@@ -0,0 +1,37 @@
+package com.pattern.creational;
+
+/**
+ * @author Karthigeyan Vellasamy
+ * Singleton Design Pattern
+ */
+public class SingletonPattern {
+	public static void main(String[] args) {
+
+		Singleton instance = Singleton.getInstance();
+		Singleton secondInstance = Singleton.getInstance();
+
+		System.out.println(instance);
+		System.out.println(secondInstance);
+	}
+}
+
+class Singleton {
+
+	private static volatile Singleton singleInstance;
+
+	private Singleton() {
+	};
+
+	public static Singleton getInstance() {
+		if (singleInstance == null) {
+			synchronized (Singleton.class) {
+				if (singleInstance == null) {
+					singleInstance = new Singleton();
+				}
+			}
+		}
+
+		return singleInstance;
+	}
+
+}
